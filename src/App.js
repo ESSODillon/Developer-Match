71d2a6d6dc9870a175adb9d75046429498f299d8
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-01-21 23:53:06 -0500

    Flipped delay

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
         });
         resetTurn();
       } else {
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
